53bd461ac64d4c6d52800bf2a8fdd80644eb053b
PeytonNP <peytonpuckett@Peytons-MacBook-Pro.local>
2019-02-09 14:22:38 -0500

    design updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <p>Replace with B^e Custom Image</p>
+    <div class="center-div">
+      <p>Query list of courses available from the database (course titles as well?)</p>
+      <h3>What course(s) do you teach?</h3>
+      <b-button-group vertical>
+        <b-button v-for="btn in buttons" :pressed.sync="btn.state" :variant="btn.variant" :key="btn.variant">
+          {{ btn.caption }}&nbsp;
+        </b-button>
+      </b-button-group>
+      <p>Selected State: <strong>{{ btnStates }}</strong></p>
+    </div>
   </div>
 </template>
 
@@ -8,6 +18,29 @@
 export default {
   name: 'home',
   components: {
+  },
+  data () {
+    return {
+      myToggle: false,
+      toggleMA161: false,
+      buttons: [
+        { variant: 'primary', caption: 'MA 16010', state: false },
+        { variant: 'primary', caption: 'MA 16020', state: false },
+        { variant: 'primary', caption: 'MA 16100', state: false },
+        { variant: 'primary', caption: 'MA 16200', state: false },
+        { variant: 'primary', caption: 'MA 16600', state: false },
+        { variant: 'primary', caption: 'MA 25000 - Linear Algebra', state: false },
+        { variant: 'primary', caption: 'MA 26100 - Multivariate Calculus', state: false },
+        { variant: 'primary', caption: 'MA 35100 - Elementary Linear Algebra', state: false },
+        { variant: 'primary', caption: 'MA 36600 - Ordinary Differential Equations', state: false },
+        { variant: 'primary', caption: 'MA 41600 - Probability', state: false }
+      ]
+    }
+  },
+  computed: {
+    btnStates () {
+      return this.buttons.map(btn => btn.state)
+    }
   }
 }
 </script>
--- src/views/Library.vue
+++ src/views/Library.vue
@@ -1,172 +1,73 @@
 <template>
-  <b-container class="py-4">
-    <b-row>
-      <b-col md="7">
-        <b-list-group v-if="questions">
-          <draggable v-model="questions" :options="{ animation: 150, group: 'question', sort: false }" >
-            <question-list-item
-              v-for="question in questions"
-              :question="question"
-              :key="question.id">
-            </question-list-item>
-          </draggable>
-        </b-list-group>
-        <b-pagination
-          class="my-3 justify-content-center"
-          size="md"
-          :total-rows="pagination.totalQuestionCount"
-          v-model="pagination.page"
-          :per-page="pagination.perpage"/>
-      </b-col>
-      <b-col md="5">
-        <b-card header="Actions" class="mb-2">
-          <p class="card-text">
-            <b-btn @click="addQuestion">Add Question</b-btn>
-          </p>
-        </b-card>
-        <div class="sticky-top">
-          <b-card header="Filters" class="my-2">
-            <p class="card-text">
-              <tag-selection v-model="filters.tags"></tag-selection>
-            </p>
-          </b-card>
-          <b-card no-body header="Exam Questions" v-if="editingExam" class="my-2">
-            <b-card-body>
-              <p class="card-text">
-                <span>You are editing exam </span>
-                <router-link :to="{ name: 'exam', params: { examID: editingExam.id }}">
-                  <strong v-text="editingExam.title"/>
-                </router-link>
-              </p>
-            </b-card-body>
-            <b-list-group flush>
-              <draggable
-                :value="editingExam.examQuestions.map(examQuestion => examQuestion.question)"
-                :options="{ animation: 150, group: 'question' }"
-                @change="examQuestionChanged"
-                id="selected-question-list">
-                <question-list-item
-                  v-for="examQuestion in editingExam.examQuestions"
-                  :question="examQuestion.question"
-                  :key="examQuestion.question.id">
-                </question-list-item>
-              </draggable>
-            </b-list-group>
-          </b-card>
-        </div>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div class="library container">
+    <br>
+    <h2>Question Catalog
+      <b-dropdown right text="Course">
+        <b-dropdown-item>All Courses</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item>MA 16100</b-dropdown-item>
+        <b-dropdown-item>MA 26500</b-dropdown-item>
+        <b-dropdown-item>Other Courses</b-dropdown-item>
+      </b-dropdown>
+    </h2>
+    <p>Select multiple courses: https://vue-multiselect.js.org/</p>
+
+    <div class="">
+      <br>
+      <b-button-group size="md">
+        <b-button>Create New Question</b-button>
+        <b-button>Create New Exam</b-button>
+        <b-button>View Exam Cart</b-button>
+      </b-button-group>
+      <br><br>
+    </div>
+
+    <div>
+      <p>Populate from database</p>
+      <table class="table table-bordered" style="width:100%">
+          <tbody>
+              <tr>
+                <td><button class="btn"><i class="fas fa-ellipsis-v"></i></button></td>
+                <td><p>Tags</p></td>
+                <td><p>Question + Answer</p></td>
+                <td><button class="btn"><i class="fas fa-eye" @click="viewMore"></i></button></td>
+              </tr>
+              <tr>
+                <td><button class="btn"><i class="fas fa-ellipsis-v"></i></button></td>
+                <td><p>Tags</p></td>
+                <td><p>Question + Answer</p></td>
+                <td><button class="btn"><i class="fas fa-eye"></i></button></td>
+              </tr>
+              <tr>
+                <td><button class="btn"><i class="fas fa-ellipsis-v"></i></button></td>
+                <td><p>Tags</p></td>
+                <td><p>Question + Answer</p></td>
+                <td><button class="btn"><i class="fas fa-eye"></i></button></td>
+              </tr>
+              <tr>
+                <td><button class="btn"><i class="fas fa-ellipsis-v"></i></button></td>
+                <td><p>Tags</p></td>
+                <td><p>Question + Answer</p></td>
+                <td><button class="btn"><i class="fas fa-eye"></i></button></td>
+              </tr>
+              <tr>
+                <td><button class="btn"><i class="fas fa-ellipsis-v"></i></button></td>
+                <td><p>Tags</p></td>
+                <td><p>Question + Answer</p></td>
+                <td><button class="btn"><i class="fas fa-eye"></i></button></td>
+              </tr>
+          </tbody>
+      </table>
+    </div>
+
+    
+  </div>
 </template>
 
 <script>
-import draggable from 'vuedraggable'
-import TagSelection from '@/components/TagSelection'
-import QuestionListItem from '@/components/QuestionListItem'
-import client from '@/utils/client'
-
 export default {
   name: 'Library',
-  data: () => ({
-    questions: null,
-    pagination: {
-      page: 1,
-      totalPageCount: null,
-      totalQuestionCount: null,
-      perpage: 15,
-    },
-    filters: {
-      tags: [],
-    }
-  }),
-  methods: {
-    loadPage () {
-      const params = new URLSearchParams()
-      params.append('page', this.pagination.page)
-      params.append('pageSize', this.pagination.perpage)
-      if (this.filters.tags && this.filters.tags.length > 0) {
-        params.append('tags', this.filters.tags.map(tag => tag.id).join(','))
-      }
-      client.get('/questions', {
-        params: params
-      })
-        .then(response => {
-          let questions = response.data
-          if (this.editingExam) {
-            questions = questions
-              .filter(question =>
-                !this.editingExam.examQuestions
-                  .some(examQuestion => examQuestion.question.id === question.id))
-          }
-          this.questions = questions
-          this.pagination.totalPageCount = parseInt(response.headers['x-total-page-count'])
-          this.pagination.totalQuestionCount = parseInt(response.headers['x-total-count'])
-        })
-    },
-    addQuestion () {
-      client.post('/questions', {
-        options: ['Default Option'],
-      })
-        .then(response => {
-          this.$router.push({
-            name: 'question',
-            params: {
-              questionID: response.data.id
-            }
-          })
-        })
-    },
-    examQuestionChanged (change) {
-      if (change.added) {
-        this.$store.commit('addExamQuestion', change.added)
-      } else if (change.removed) {
-        this.$store.commit('removeExamQuestion', change.removed.oldIndex)
-      } else if (change.moved) {
-        this.$store.commit('removeExamQuestion', change.moved.oldIndex)
-        this.$store.commit('addExamQuestion', change.moved)
-      }
-    }
-  },
-  mounted () {
-    this.loadPage()
-  },
-  watch: {
-    'pagination.page' (page) {
-      this.loadPage(page)
-    },
-    'filters.tags' (tags) {
-      this.pagination.page = 1
-      this.loadPage(1)
-    }
-  },
-  computed: {
-    editingExam () {
-      return this.$store.state.editingExam
-    },
-  },
   components: {
-    TagSelection,
-    QuestionListItem,
-    draggable,
   }
 }
 </script>
-
-<style lang="scss">
-.library-badge-container {
-  .badge {
-    margin-left: 0.25rem;
-    margin-right: 0.25rem;
-    &:first-child {
-      margin-left: 0;
-    }
-    &:last-child {
-      margin-right: 0;
-    }
-  }
-}
-
-#selected-question-list{
-  min-height: 10rem;
-}
-</style>
